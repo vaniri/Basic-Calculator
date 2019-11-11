178a4c6a96f336831b001bafeca01bd50325b5f8
Vaniri <foxifennec@gmail.com>
2019-11-10 22:36:55 -0500

    finally make it work

--- calculator.js
+++ calculator.js
@@ -1,43 +1,65 @@
-alert("Hi!");
+let numPress = document.getElementsByClassName("number");
+    let operPress = document.getElementsByClassName("operator");
+    let allNumPress = document.getElementById("result").innerHTML;
+    let prevNum = 0;
+    let prevOper = null;
+    let clearOperWind = false;
 
-let numVar = document.getElementsByClassName("number");
-let operVal = document.getElementsByClassName("operator");
 
-function addEventForNum(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i].addEventListener("click", function() {
-      let allNumStr = (document.getElementById("result").innerHTML += arr[i].value);
+    document.getElementById("button-equal").addEventListener("click", function () {
+      performPrevOp();
+      prevNum = 0;
+      prevOper = null;
+    })
+
+    document.getElementById("button-clear").addEventListener("click", function () {
+      document.getElementById("result").innerHTML = "";
+      clearOperWind = false;
     });
-  }
-}
 
-function addEventForOper(arr, firstNum, secondNum) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i].addEventListener("click", function() {
-      let buttonVal = arr[i].value;
-      let result = 0;
-      if (buttonVal == "plus") {
-        result = firstNum + secondNum;
-      } else if (buttonVal == "minus") {
-        result = firstNum - secondNum;
-      } else if (buttonVal * "times") {
-        result = firstNum + secondNum;
-      } else if (buttonVal == "divide") {
-        result = firstNum / secondNum;
-      } else if (buttonVal == "equals") {
-        return;
+
+    for (let i = 0; i < numPress.length; i++) {
+      numPress[i].addEventListener("click", function () {
+        if (clearOperWind) {
+          clearResult();
+          clearOperWind = false;
+        }
+        document.getElementById("result").innerHTML += numPress[i].value;
+      });
+    }
+
+    for (let i = 0; i < operPress.length; i++) {
+      operPress[i].addEventListener("click", function () {
+        if (prevOper == null) {
+          prevOper = operPress[i].value;
+          prevNum = readResult();
+        } else { 
+          performPrevOp(); 
+          prevOper = operPress[i].value;
+        }
+        clearOperWind = true;
+      });
+    }
+
+    function performPrevOp() {
+      if (prevOper == "plus") {
+        prevNum += readResult();
+      } else if (prevOper == "times") {
+        prevNum *= readResult();
+      } else if (prevOper == "minus") {
+        prevNum -= readResult();
+      } else if (prevOper == "divide") {
+        prevNum /= readResult();
+      } else if (prevOper == "power") {
+        prevNum = Math.pow(prevNum, readResult());
       }
-      return result;
-    });
-  }
-}
+      document.getElementById("result").innerHTML = prevNum;
+    }
 
-addEventForNum(numVar);
-addEventForOper(operVal);
+    function readResult() {
+      return +(document.getElementById("result").innerHTML);
+    }
 
-function eventHendler() {
-  allNum = +allNumStr;
-  document.getElementById("button-clear").addEventListener("click", function() {
-    document.getElementById("result").innerHTML = "";
-  });
-}
+    function clearResult() {
+      document.getElementById("result").innerHTML = "";
+    }
